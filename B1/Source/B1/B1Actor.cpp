3518a5de180f4a9305227893de3a82b39786164c
Raphael <rookie17@naver.com>
2025-03-14 09:10:04 +0900

    Created B1Actor

---
+++ B1/Source/B1/B1Actor.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "B1Actor.h"
+
+// Sets default values
+AB1Actor::AB1Actor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AB1Actor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// 경기게임마이스터고등학교
+}
+
+// Called every frame
+void AB1Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
