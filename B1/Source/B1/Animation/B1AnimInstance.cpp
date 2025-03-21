cee2b4da3039c8a027fb475eaabd7e77341171b5
Raphael <rookie17@naver.com>
2025-03-21 11:59:20 +0900

    Create Hello Animation

---
+++ B1/Source/B1/Animation/B1AnimInstance.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/B1AnimInstance.h"
+
+UB1AnimInstance::UB1AnimInstance()
+{
+}
+
+void UB1AnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+}
+
+void UB1AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+}
