65d6e08233389be02302394744d24a66fc32c7e7
Raphael <rookie17@naver.com>
2025-03-21 12:54:41 +0900

    Create Animation Blueprint

--- B1/Source/B1/Animation/B1AnimInstance.cpp
+++ B1/Source/B1/Animation/B1AnimInstance.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Animation/B1AnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Character/B1CharacterBase.h"
 
 UB1AnimInstance::UB1AnimInstance()
 {
@@ -10,9 +13,27 @@ UB1AnimInstance::UB1AnimInstance()
 void UB1AnimInstance::NativeInitializeAnimation()
 {
 	Super::NativeInitializeAnimation();
+
+	Character = Cast<AB1CharacterBase>(TryGetPawnOwner());
+
+	if (Character)
+	{
+		MovementComponent = Character->GetCharacterMovement();
+	}
 }
 
 void UB1AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
 	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Character == nullptr)
+		return;
+
+	if (MovementComponent == nullptr)
+		return;
+
+	Velocity = MovementComponent->Velocity;
+	GroundSpeed = Velocity.Size2D();
+	bShouldMove = (GroundSpeed > 3.0f && MovementComponent->GetCurrentAcceleration() != FVector::ZeroVector);
+	bIsFalling = MovementComponent->IsFalling();
 }
--- B1/Source/B1/Animation/B1AnimInstance.h
+++ B1/Source/B1/Animation/B1AnimInstance.h
@@ -21,4 +21,23 @@ public:
 	virtual void NativeInitializeAnimation() override;
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<class AB1CharacterBase> Character;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<class UCharacterMovementComponent> MovementComponent;
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	FVector Velocity = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly)
+	float GroundSpeed = 0.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bShouldMove = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsFalling = false;
 };
