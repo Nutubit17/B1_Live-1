18b01d4da6cea18e66896dfab39cf84913b23691
Raphael <rookie17@naver.com>
2025-03-31 14:40:56 +0900

    Create B1Box Class

---
+++ B1/Source/B1/Prop/B1Box.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Prop/B1Box.h"
+
+// Sets default values
+AB1Box::AB1Box()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AB1Box::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AB1Box::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
